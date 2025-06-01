509df41c62dd49d04e98aba296ca53a21febeb6c
al_trapeznikov <al.trapeznikov.743@gmail.com>
2025-06-01 05:16:47 +0400

    expiresIn -> 1h

--- src/auth/adapters/jwt.adapter.ts
+++ src/auth/adapters/jwt.adapter.ts
@@ -1,18 +1,19 @@
-import jwt, {SignOptions} from 'jsonwebtoken';
+import jwt, {JwtPayload, SignOptions} from 'jsonwebtoken';
 import {config} from '../../core/settings/config';
 import {UnauthorizedError} from '../../core/errors';
 
 export const jwtService = {
   async createToken(userId: string): Promise<string> {
     return jwt.sign({userId}, config.AC_SECRET, {
-      expiresIn: config.AC_TIME
+      expiresIn: '1h' // config.AC_TIME
     } as SignOptions);
   },
 
-  async verifyToken(token: string): Promise<{userId: string} | null> {
+  async verifyToken(token: string): Promise<JwtPayload> {
     try {
-      return jwt.verify(token, config.AC_SECRET) as {userId: string};
+      return jwt.verify(token, config.AC_SECRET) as JwtPayload;
     } catch (error) {
+      console.log('Verify token error');
       throw new UnauthorizedError('Incorrect login or password');
     }
   }
