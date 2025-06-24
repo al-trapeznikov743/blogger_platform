098605c341601d9b512404d7c64c6137eea738c2
al_trapeznikov <al.trapeznikov.743@gmail.com>
2025-06-24 23:42:26 +0400

    email confirm

--- src/core/settings/config.ts
+++ src/core/settings/config.ts
@@ -13,6 +13,9 @@ export const config: {
   AC_SECRET: process.env.AC_SECRET || 'ghfsd@q23dwdidwecee2',
   AC_TIME: process.env.AC_TIME || '10s',
 
-  EMAIL: process.env.EMAIL || 'your-email@gmail.com',
-  EMAIL_PASS: process.env.EMAIL_PASS || 'your-app-password'
+  // EMAIL: process.env.EMAIL || 'your-email@gmail.com',
+  // EMAIL_PASS: process.env.EMAIL_PASS || 'your-app-password'
+
+  EMAIL: process.env.EMAIL || 'al.trapeznikov.744@gmail.com',
+  EMAIL_PASS: process.env.EMAIL_PASS || 'brsm vhqd ckwh lxpu'
 };
